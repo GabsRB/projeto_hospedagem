69bdbe855548b5b97aac5a75d4152d664b42f303
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2024-07-25 13:47:14 -0300

    Add files via upload

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,15 @@
+class PageController < ApplicationController
+    before_action :require_login
+  
+    def dashboard
+      @user = current_user
+    end
+  
+    private
+  
+    def require_login
+      unless session[:user_id]
+        redirect_to login_path, alert: "Você deve estar logado para entrar"
+      end
+    end
+  end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path, notice: "Entrada com sucesso!"
+    else
+      flash.now[:alert] = "E-mail ou senha invalida. Tente NOvamente!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Ação de saida com sucesso!"
+  end
+end
